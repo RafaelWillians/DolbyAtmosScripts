617921df6f6ade35426d3b832e73d72fe69d1c12
Rafael Silva Willians <rafael.willians@outlook.com>
2024-09-25 18:15:45 +0000

    Adicionada info no script

--- scripts/KZ-DFI.bat
+++ scripts/KZ-DFI.bat
@@ -1,10 +1,18 @@
+:: Script para habilitar o Dolby Atmos na saída de áudio KZ-D-FI.
+:: Este script não irá definir essa saída por padrão, 
+:: pois o padrão sempre vai ser o Voicemeeter Input.
 
+:: Foi necessário ajustar o script para desabilitar e reabilitar o dispositivo,
+:: além de incluir delays devido a eventuais falhas em aplicar as configurações.
 
 @echo off
 
+:: Define como padrão o Voicemeeter Input
 SoundVolumeView /SetDefault "Voicemeeter Input" all
 timeout /t 0 /nobreak >nul
 
+:: Necessário definir outros dispositivos com outro áudio espacial
+:: devido a inconsistências na execução do script 
 SoundVolumeView /SetSpatial "CORSAIR VOID ELITE Wireless Gaming Headset" "Windows Sonic"
 timeout /t 0 /nobreak >nul
 
@@ -20,12 +28,15 @@ timeout /t 0 /nobreak >nul
 SoundVolumeView /SetListenToThisDevice "CABLE Output" 1
 timeout /t 0 /nobreak >nul
 
+:: Desabilita e reabilita, devido a falhas para retirar Dolby Atmos 
+:: do dispositivo anterior.
 SoundVolumeView /Disable "KZ_DFI"
 timeout /t 0 /nobreak >nul
 
 SoundVolumeView /Enable "KZ_DFI"
 timeout /t 0 /nobreak >nul
 
+:: Reexecuta o script, para garantir êxito na alteração
 SoundVolumeView /SetDefault "Voicemeeter Input" all
 timeout /t 0 /nobreak >nul
 
@@ -44,7 +55,7 @@ timeout /t 0 /nobreak >nul
 SoundVolumeView /SetListenToThisDevice "CABLE Output" 1
 timeout /t 0 /nobreak >nul
 
-
+:: Mensagem na tela para informar a alteração com sucesso.
 echo Alterado audio para KZ-D FI
 timeout /t 3 /nobreak >nul
 
